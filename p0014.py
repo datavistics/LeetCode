e56d5c02f9545482b4edb3da2850d10c076198e1
datavistics <datavistics@gmail.com>
2018-02-17 23:08:54 +0400

    Solved

---
+++ p0014.py
@@ -0,0 +1,13 @@
+def longestCommonPrefix(strs):
+    """
+    :type strs: List[str]
+    :rtype: str
+    """
+    if not strs: return ''
+    s1 = min(strs)
+    s2 = max(strs)
+    for i, c in enumerate(s1):
+        if c != s2[i]:
+            return s1[:i]
+    return s1
+
