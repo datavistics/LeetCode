0598e2ea2f6291a09ed12bb020b544a447396a9f
Derek Thomas <dthomas@coda.ae>
2021-07-02 20:48:07 +0400

    Adding 202

---
+++ leetcode/editor/en/[202]Happy Number.py
@@ -0,0 +1,79 @@
+# Write an algorithm to determine if a number n is happy. 
+# 
+#  A happy number is a number defined by the following process: 
+# 
+#  
+#  Starting with any positive integer, replace the number by the sum of the squa
+# res of its digits. 
+#  Repeat the process until the number equals 1 (where it will stay), or it loop
+# s endlessly in a cycle which does not include 1. 
+#  Those numbers for which this process ends in 1 are happy. 
+#  
+# 
+#  Return true if n is a happy number, and false if not. 
+# 
+#  
+#  Example 1: 
+# 
+#  
+# Input: n = 19
+# Output: true
+# Explanation:
+# 12 + 92 = 82
+# 82 + 22 = 68
+# 62 + 82 = 100
+# 12 + 02 + 02 = 1
+#  
+# 
+#  Example 2: 
+# 
+#  
+# Input: n = 2
+# Output: false
+#  
+# 
+#  
+#  Constraints: 
+# 
+#  
+#  1 <= n <= 231 - 1 
+#  
+#  Related Topics Hash Table Math Two Pointers 
+#  👍 3388 👎 546
+
+
+# leetcode submit region begin(Prohibit modification and deletion)
+class Solution:
+    def isHappy(self, n: int) -> bool:
+        d = set()
+        d.add(n)
+
+        def get_square_sum(n):
+            square_sum = 0
+            div = n
+
+            while True:
+                if not div:
+                    break
+                div, rem = divmod(div, 10)
+                square_sum += rem**2
+            print(n, square_sum)
+            return square_sum
+        while True:
+            n = get_square_sum(n)
+            if n == 1:
+                return True
+            elif n in d:
+                return False
+            d.add(n)
+
+
+# leetcode submit region end(Prohibit modification and deletion)
+
+s = Solution()
+res = s.isHappy(7)
+print(res)
+
+# Success:
+# Runtime:36 ms, faster than 61.67% of Python3 online submissions.
+# Memory Usage:14.5 MB, less than 14.73% of Python3 online submissions.
