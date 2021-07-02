03f2dea0254e32954c7da9ccabfe6f4701e4d559
Derek Thomas <dthomas@coda.ae>
2021-07-02 21:43:39 +0400

    Adding 21

---
+++ leetcode/editor/en/[21]Merge Two Sorted Lists.py
@@ -0,0 +1,66 @@
+# Merge two sorted linked lists and return it as a sorted list. The list should 
+# be made by splicing together the nodes of the first two lists. 
+# 
+#  
+#  Example 1: 
+# 
+#  
+# Input: l1 = [1,2,4], l2 = [1,3,4]
+# Output: [1,1,2,3,4,4]
+#  
+# 
+#  Example 2: 
+# 
+#  
+# Input: l1 = [], l2 = []
+# Output: []
+#  
+# 
+#  Example 3: 
+# 
+#  
+# Input: l1 = [], l2 = [0]
+# Output: [0]
+#  
+# 
+#  
+#  Constraints: 
+# 
+#  
+#  The number of nodes in both lists is in the range [0, 50]. 
+#  -100 <= Node.val <= 100 
+#  Both l1 and l2 are sorted in non-decreasing order. 
+#  
+#  Related Topics Linked List Recursion 
+#  👍 7318 👎 804
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+# leetcode submit region begin(Prohibit modification and deletion)
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
+        dummy = cur = ListNode()
+        while l1 and l2:
+            # Add node which is lesser
+            if l1.val < l2.val:
+                cur.next = l1
+                l1 = l1.next
+            else:
+                cur.next = l2
+                l2 = l2.next
+            cur = cur.next  # Important to increment cur as we iterate
+        cur.next = l1 or l2  # Gets the last one
+        return dummy.next
+# leetcode submit region end(Prohibit modification and deletion)
+
+# Success:
+# Runtime:32 ms, faster than 92.14% of Python3 online submissions.
+# Memory Usage:14 MB, less than 95.36% of Python3 online submissions
