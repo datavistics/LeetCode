1a4e39b813f83d4928ab582df20d1518172867f4
Derek Thomas <dthomas@coda.ae>
2021-07-04 10:01:56 +0400

    Update when we rename

--- leetcode.ts
+++ leetcode.ts
@@ -2,17 +2,13 @@
 var config = new Object();
 config["leetcode_path"] = "/Users/derekthomas/projects/LeetCode/leetcode/editor/en/";
 
-// Calculate paths
+// Calculate paths  
 let python_file_name = await tp.system.clipboard();
 let md_file_name = python_file_name.replace(".py", ".md");
 let python_path = config["leetcode_path"] + python_file_name;
 let md_path = config["leetcode_path"] + md_file_name;
 
-// Rename Obsidian filename
-let ob_file_name = python_file_name.replace(".py", "").replace('[', '').replace(']', ' - ');
-tp.file.rename(ob_file_name);
-
-// Read files
+// Read files  
 function get_text(path) {
     const fs = require('fs');
 
@@ -27,7 +23,7 @@ function get_text(path) {
 var original = get_text(python_path);
 var md = get_text(md_path);
 
-// Process python file
+// Process python file  
 let mdSplitIndex = original.indexOf('\n\n');
 let end_split_index = original.indexOf('\n# Success:');
 
@@ -43,7 +39,7 @@ function get_regex(regex, input_str) {
     return input_match[0][1];
 }
 
-// Parse html topics
+// Parse html topics  
 const topics_re = /<div><div>Related Topics<\/div><div>.+<\/div><\/div>/g;
 topic_match = [...md.matchAll(topics_re)];
 var parser = new DOMParser();
@@ -58,21 +54,21 @@ for (let item of liTags) {
 }
 
 
-// Regexs for vars
+// Regexs for vars  
 const runtime_re = /Runtime:(\d+ ms)/g
 const faster_re = /faster than ([^ ]+)/g
 const memory_re = /Memory Usage:(.+?),/g
 const less_re = /less than ([^ ]+)/g
 const time_re = /Time:: (.+)/g
 
-// Optional regex
+// Optional regex  
 try{
     var total_time = get_regex(time_re, end_cleanup);
 }catch(error){
     var total_time='';
 }
 
-// Outputting to file
+// Outputting to file  
 tR += `---\n`;
 tR += `tags: ${tags}\n`
 tR += `fileType: LeetCode\n`;
@@ -87,3 +83,7 @@ tR += `Memory:: ${get_regex(memory_re, end_cleanup)}\n`;
 tR += `Memory Less Than:: ${get_regex(less_re, end_cleanup)}\n`;
 tR += total_time ? `Time:: ${total_time}\n` : '';
 tR += `# Notes\n`;
+
+// Rename Obsidian filename  
+let ob_file_name = python_file_name.replace(".py", "").replace('[', '').replace(']', ' - ');
+tp.file.rename(ob_file_name);
