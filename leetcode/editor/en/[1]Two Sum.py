bc3e3ead837e0ca82c126ae824e3d64164961fa0
datavistics <datavistics@gmail.com>
2021-07-12 09:59:45 -0500

    Adding 1

--- leetcode/editor/en/[1]Two Sum.py
+++ leetcode/editor/en/[1]Two Sum.py
@@ -41,17 +41,54 @@
 #  Related Topics Array Hash Table 
 #  👍 20051 👎 706
 
-
+from typing import List
+import bisect
 # leetcode submit region begin(Prohibit modification and deletion)
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        # Create dict of differences
-        d = {target - k: i for i, k in enumerate(nums)}
+        # # Create dict of differences
+        # d = {target - k: i for i, k in enumerate(nums)}
+        #
+        # # Return difference indices
+        # for i, k in enumerate(nums):
+        #     v = d.get(k)
+        #     if v is not None and v != i:
+        #         if i < v:
+        #             return [i,v]
+        #         else:
+        #             return [v,i]
+        temp = sorted(nums.copy())
+        for i, num in enumerate(temp):
+            bis = bisect.bisect_left(temp, target - num, lo=i + 1)
+            j = bis if bis < len(nums) - 1 else len(nums) - 1
+            if num + temp[j] == target:
+                look_1 = temp[i]
+                look_2 = temp[j]
+                out = []
+                for i, num in enumerate(nums):
+                    if num == look_1:
+                        out.append(i)
+                        if len(out) == 2:
+                            return out
+                        else:
+                            continue
+                    if num == look_2:
+                        out.append(i)
+                        if len(out) == 2:
+                            return out
+                        else:
+                            continue
+                return out
 
-        # Return difference indices
-        for i, k in enumerate(nums):
-            v = d.get(k)
-            if v is not None and v != i:
-                return [min(i, v), max(i, v)]
-        
 # leetcode submit region end(Prohibit modification and deletion)
+
+
+# s = Solution.twoSum(None,[3,2,4], 6)
+s = Solution.twoSum(None,[3,3], 6)
+# s = Solution.twoSum(None, [3, 2, 3], 6)
+# s = Solution.twoSum(None, [-1, -2, -3, -4, -5], -8)
+print(s)
+
+# Success:
+# Runtime: 64 ms, faster than 53.38% of Python3 online submissions.
+# Memory Usage: 15 MB, less than 61.09% of Python3 online submissions.
