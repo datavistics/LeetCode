ebadeaba1718e3e72e41233d42c9a026422f91fe
datavistics <datavistics@gmail.com>
2021-07-12 11:20:00 -0500

    Adding 36

--- timeit_test.py
+++ timeit_test.py
@@ -1,6 +1,6 @@
 from timeit import timeit
 
-problem = '[15]3Sum.py'
+problem = '[36]Valid Sudoku.py'
 s = fr"""
 path = r'C:\Users\ditho\Projects\LeetCode\leetcode\editor\en\{problem}'
 import importlib.util
@@ -11,6 +11,6 @@ spec.loader.exec_module(foo)
 
 num = 30
 sol_in = [0,0,0]
-t = timeit(f"s = foo.Solution.threeSum(None, {sol_in})", setup=s, number=num)
+t = timeit(f"s = foo.Solution.isValidSudoku(None, {sol_in})", setup=s, number=num)
 print(t/num)
 
